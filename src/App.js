68b0bec48eaea645b1ccd5b0fe76b338b3c94c18
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-04-05 09:10:34 +0100

    color commit

--- src/App.js
+++ src/App.js
@@ -14,69 +14,69 @@ const App = () => {
 
     fetchData(); // Initial fetch
 
-    const interval = setInterval(fetchData, 5000); // Fetch every 5 seconds
+    const interval = setInterval(fetchData, 5000); 
 
-    return () => clearInterval(interval); // Cleanup function
+    return () => clearInterval(interval);
 
   }, []);
 
   return (
     <div className='app'>
-      <h1 style={{ textAlign: 'center', fontFamily:'monospace', fontWeight:'bolder', paddingTop:'50px', paddingBottom:'30px', marginLeft:'130px', marginRight:'130px', marginTop:'50px',border:'3px solid #dbdac5', borderRadius:'15px'}}>DATABASE CARDS</h1>
-      <div className="container" style={{ paddingTop: '50px' }}>
+      <h1 style={{ textAlign: 'center', fontFamily:'monospace', fontWeight:'bolder', paddingTop:'20px', paddingBottom:'30px', marginTop:'50px', overflowX:'hidden'}}>DATABASE CARDS</h1>
+      <div className="container" style={{ paddingTop: '20px' }}>
         {data.slice(0, 6).map((d, i) => (
           <div className="row" key={i}>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body" style={{backgroundColor:'#e3d8e6'}}>
-                  <h5 className="card-title" style={{textAlign:'center'}}>Time</h5>
+                <div className="card-body" style={{backgroundColor:'#f3f2ff'}}>
+                  <h5 className="card-title" style={{textAlign:'center', fontWeight:'bolder'}}>TIME</h5>
                   <hr />
-                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px'}}>{d.time}</p>
+                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px', fontWeight:'bolder', fontSize:'17px'}}>{d.time}</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body" style={{backgroundColor:'#b9edd4'}}>
-                  <h5 className="card-title" style={{textAlign:'center'}}>Temperature</h5>
+                <div className="card-body" style={{backgroundColor:'#f2fff4'}}>
+                  <h5 className="card-title" style={{textAlign:'center', fontWeight:'bolder'}}>TEMPRATURE</h5>
                   <hr />
-                  <p className="card-text"  style={{textAlign:'center', paddingTop:'50px'}}>{d.temprature}</p>
+                  <p className="card-text"  style={{textAlign:'center', paddingTop:'50px', fontWeight:'bolder', fontSize:'20px'}}>{d.temprature}</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body" style={{backgroundColor:'#f7f28f',}}>
-                  <h5 className="card-title" style={{textAlign:'center'}}>Relative Humidity</h5>
+                <div className="card-body" style={{backgroundColor:'#fffcf2',}}>
+                  <h5 className="card-title" style={{textAlign:'center',fontWeight:'bolder'}}>RELATIVE HUMIDITY</h5>
                   <hr />
-                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px'}}>{d.relativehumidity}</p>
+                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px',fontWeight:'bolder', fontSize:'20px'}}>{d.relativehumidity}</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body">
-                  <h5 className="card-title" style={{textAlign:'center'}}>Pressure</h5>
+                <div className="card-body" style={{backgroundColor:'#fcf2ff'}}>
+                  <h5 className="card-title" style={{textAlign:'center', fontWeight:'bolder'}}>PRESSURE</h5>
                   <hr />
-                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px'}}>{d.pressure}</p>
+                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px',fontWeight:'bolder', fontSize:'20px'}}>{d.pressure}</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body">
-                  <h5 className="card-title" style={{textAlign:'center'}}>Altitude</h5>
+                <div className="card-body" style={{backgroundColor:'#fffcf2'}}>
+                  <h5 className="card-title" style={{textAlign:'center', fontWeight:'bolder'}}>ALTITUDE</h5>
                   <hr />
-                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px'}}>{d.altitude}</p>
+                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px',fontWeight:'bolder', fontSize:'20px'}}>{d.altitude}</p>
                 </div>
               </div>
             </div>
             <div className="col-md-4 col-sm-6">
               <div className="card" style={{ border: '2px solid black' }}>
-                <div className="card-body">
-                  <h5 className="card-title" style={{textAlign:'center'}}>Oxygen Level</h5>
+                <div className="card-body" style={{backgroundColor:'#fff2f3'}}>
+                  <h5 className="card-title" style={{textAlign:'center', fontWeight:'bolder'}}>OXYGEN LEVEL</h5>
                   <hr />
-                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px'}}>{d.oxygenlevel}</p>
+                  <p className="card-text" style={{textAlign:'center', paddingTop:'50px',fontWeight:'bolder', fontSize:'20px'}}>{d.oxygenlevel}</p>
                 </div>
               </div>
             </div>
